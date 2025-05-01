c97c12f49b56f311dd37183bc35f8d875865c78f
HKLMFernando <matheeshafernando61@gmail.com>
2025-05-01 11:21:46 +0530

    updates

---
+++ model/CustomerModel.js
@@ -0,0 +1,9 @@
+export default class CustomerModel {
+    constructor(fname, lname, address,email,contact) {
+        this.firstname = fname;
+        this.lastname = lname;
+        this.address = address;
+        this.email =email;
+        this.contact = contact;
+    }
+}
