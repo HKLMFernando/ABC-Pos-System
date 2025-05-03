11c3432b2411faca5546d8d47c37733a78d6cd29
HKLMFernando <matheeshafernando61@gmail.com>
2025-05-03 08:31:51 +0530

    updates

--- controller/ItemControlller.js
+++ controller/ItemControlller.js
@@ -3,7 +3,7 @@ import {Item_db} from "../db/DB.js";
 import ItemModel from "../model/CustomerModel.js";
 
 
-// load student records
+
 function loadItem() {
     $('#item-tbody').empty();
     Item_db.map((item, index) => {
@@ -26,4 +26,49 @@ function loadItem() {
         $('#item-tbody').append(data);
     })
 }
+
+// save
+$('#itemSave').on('click', function(){
+    // let fname = document.getElementById('fname').value;
+    let ItemCode = $('#ItemCode').val();
+    let ItemName = $('#ItemName').val();
+    let QtyOnHand = $('#QtyOnHand').val();
+    let PricePerUnit = $('#PricePerUnit').val();
+
+    if(ItemCode === '' || ItemName === '' || QtyOnHand === '' || PricePerUnit === '' ) {
+        // alert("Invalid inputs!");
+
+        Swal.fire({
+            title: 'Error!',
+            text: 'Invalid Inputs',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+        })
+    } else {
+
+
+        // let student_data = {
+        //     fname: fname,
+        //     lname: lname,
+        //     address: address
+        // };
+
+        let Item_data = new ItemModel(ItemCode ,ItemName ,QtyOnHand ,PricePerUnit);
+
+        Item_db.push(Item_data);
+
+        console.log(Item_data);
+
+        loadItem();
+
+
+        Swal.fire({
+            title: "Added Successfully!",
+            icon: "success",
+            draggable: true
+        });
+        clearForm();
+
+    }
+});
 
