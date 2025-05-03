696a780bfd2cf8e72b4213532dd6c1ee519c3626
HKLMFernando <matheeshafernando61@gmail.com>
2025-05-03 08:12:46 +0530

    updates

---
+++ model/ItemModel.js
@@ -0,0 +1,9 @@
+export default class ItemModel {
+    constructor(ItemId, ItemName, QtyOnHand,PricePerUnit) {
+        this.ItemId = ItemId;
+        this.ItemName = ItemName;
+        this.QtyOnHand =QtyOnHand;
+        this.PricePerUnit = PricePerUnit;
+
+    }
+}
