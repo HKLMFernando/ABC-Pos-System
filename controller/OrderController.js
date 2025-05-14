c0214d39e335fe4b5de989997170964ed4fdb321
HKLMFernando <matheeshafernando61@gmail.com>
2025-05-14 19:09:25 +0530

    updates

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -5,3 +5,35 @@ import CustomerModel from "../model/CustomerModel.js";
 import OrderModel from "../model/OrderModel.js";
 import ItemModel from "../model/ItemModel.js";
 
+
+// searchCustomer
+$('#searchCustomer').on('click',function () {
+    searchCustomer();
+})
+
+function searchCustomer() {
+    let id = $('#searchCustomerInput').val().trim();
+    if (!id){
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Search an ID first",
+        });
+        return
+    }
+    const c = Customer_db.find(cust => cust.customerId === id);
+    if (c){
+        $('#loadCid').val(c.customerId);
+        $('#loadFirstName').val(c.firstname);
+        $('#loadLastName').val(c.lastname)
+        $('#loadAddress').val(c.address);
+        $('#loadEmail').val(c.email);
+        $('#loadContact').val(c.contact);
+    }else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Id does not Exist",
+        });
+    }
+}
